dc02c03d0bbe51f0f6e43733dbb37fb28602de8c
deadlift1226 <danemorgan91@gmail.com>
2018-11-02 00:39:14 +0000

    fix join

--- testing.py
+++ testing.py
@@ -1,3 +1,4 @@
+from time import sleep
 from subprocess import check_output, run
 from datetime import datetime
 
@@ -16,26 +17,23 @@ def digitize_ip(ip_addr):
 def rebuild_ip(octets):
     ip_addr = ''
     stringified = [str(x) for x in octets]
-
-    for s in stringified:
-        if s == stringified[0]:
-            ip_addr = s
-        else:
-            ip_addr = ip_addr + '.' + s 
-    return ip_addr
+    
+    return '.'.join(stringified)
 
 def system_ping(ip_addr):
-    cmd = 'ping -W .25 -c 1 ' + ip_addr 
+    cmd = 'ping -W 1 -c 1 ' + ip_addr 
     try:
 
         x = str(check_output(cmd, shell=True))
+        print(x)
         if '1 received' not in x:
             print(Fore.RED + ip_addr + Style.RESET_ALL + ' no reply')
-            return ip_addr + ' no reply'
+            return ip_addr + ' no reply ' + str(datetime.now()) + '\n'
+
 
         else:
             print(Fore.GREEN + ip_addr + Style.RESET_ALL + ' replied')
-            return ip_addr + ' replied'
+            return ip_addr + ' replied ' + str(datetime.now()) + '\n'
 
     except KeyboardInterrupt:
         exit()
@@ -46,36 +44,47 @@ def system_ping(ip_addr):
                 print(Fore.RED + ip_addr + Style.RESET_ALL + ' no reply')
             return ip_addr + ' no reply'
 
-def lots_of_pings(start, end, outfile):
+def lots_of_pings(start, end, outfile, public = True):
     s0, s1, s2, s3 = digitize_ip(start)
     e0, e1, e2, e3 = digitize_ip(end)
     s = [ s0, s1, s2, s3 ]
     e = [ e0 , e1, e2, e3 ]
     #_e = [ e0+1 , e1+1, e2+1, e3+1 ]
-    print(s,e)
-    print(s3, e3)
-    
+    #print(s,e)
+    s0_reserved =  [ 10, 100, 127, 169, 172, 192, 198, 203, 224, 240, ]
+    s1_reserved = 64
+    s2_reserved = 2
+    s3_reserved = 5 
     for i in range(s0, e0+1):
-        #for i in range(s2, 256):
+        #if ( s0 in s0_reserved ):
+        #    continue
         s[0] = i
         for j in range(s1, e1+1):
-        #for i in range(s2, 256):
             s[1] = j
 
             for k in range(s2, e2+1):
-            #for i in range(s2, 256):
                 s[2] = k
-                #for i in range(s3, e3+1):
-                for l in range(s3, 256):
-                    s[3] = l  
+
+                for l in range(s3, e3+1):
+                    #s[3] = l  
+                    s[3] = l
+                    #print(s)
+                    #x  = rebuild_ip(s)
+                    #sleep(.001)
+                    #x = system_ping(host)
                     host  = rebuild_ip(s)
-                    x = system_ping(host)
-                    if x:
-                        text = x + str(datetime.now()) + '/n'
-                        write_to_file(text , outfile)
-                    else:
-                        continue
-sip = '192.168.0.1'
-eip = '192.168.1.255'
+                    print(host)
+                    #write_to_file(host , outfile)
+
+                host  = rebuild_ip(s)
+                print(host)
+            host  = rebuild_ip(s)
+            print(host)
+        host  = rebuild_ip(s)
+        print(host)
+
+sip = '1.0.0.0'
+eip = '1.0.2.255'
+#eip = '249.255.255.255'
 outfile = 'sweep' + datetime.now().strftime('_%d_%b_%y') + '.txt'
 lots_of_pings(sip, eip, outfile)
